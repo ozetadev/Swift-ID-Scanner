28e3c415773a1542352d54c3e7ad3ad7eda26047
Philip Bernstein <ozetadev@me.com>
2016-11-22 00:56:08 -0500

    initial commit

--- swiftscanner/ViewController.swift
+++ swiftscanner/ViewController.swift
@@ -7,12 +7,57 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
 
-class ViewController: UIViewController {
+class ViewController<AVCaptureMetadataOutput>: UIViewController {
 
+    var captureSession:AVCaptureSession?
+    var captureDevice:AVCaptureDevice?
+    var captureInput:AVCaptureInput?
+    var metadataOutput:AVCaptureMetadataOutput?
+    var metadataInput:AVCaptureMetadataInput?
+    var previewLayer:AVCaptureVideoPreviewLayer?
+    var highlightPreview:UIView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.configureSession()
+        self.configureDevice()
+        self.configureInput()
+        self.configureOutput()
+    }
+    
+    func configureSession() {
+        captureSession = AVCaptureSession()
+        captureSession?.sessionPreset = AVCaptureSessionPresetHigh;
+    }
+    
+    func configureDevice() {
+        captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        do {
+            try captureDevice?.lockForConfiguration()
+        } catch {
+            print("Something went wrong!")
+        }
+        captureDevice?.videoZoomFactor = 3.5;
+        captureDevice?.unlockForConfiguration()
+    }
+    
+    func configureInput() {
+        do {
+            try captureInput = AVCaptureDeviceInput(device: captureDevice)
+            
+        }
+        catch {
+            NSLog("Error adding input");
+        }
+        
+        captureSession?.addInput(captureInput)
+    }
+    
+    func configureOutput() {
     }
 
     override func didReceiveMemoryWarning() {
